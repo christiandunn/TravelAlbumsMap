92e61c3e761d1672e574c8e87951d7de3b91972b
Christian Dunn <christianmdunn@gmail.com>
2016-04-19 18:53:59 -0700

    Initial Commit

---
+++ MyPhotosOnAMap/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyPhotosOnAMap
+//
+//  Created by Christian Dunn on 4/19/16.
+//  Copyright © 2016 Christian Dunn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyPhotosOnAMap/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyPhotosOnAMap
+//
+//  Created by Christian Dunn on 4/19/16.
+//  Copyright © 2016 Christian Dunn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
