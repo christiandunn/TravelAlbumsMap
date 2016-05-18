1d1a19047d198b89608fded9489c9e98fcae47ae
Guest User <Guest@christians-mac-mini.local>
2016-05-17 22:19:24 -0700

    Fix bug where library loader doesn't work on non-initialized or partially-initialized Photos.app library

--- MyPhotosOnAMap/MediaLibraryAccessor.h
+++ MyPhotosOnAMap/MediaLibraryAccessor.h
@@ -10,17 +10,33 @@
 #import <MediaLibrary/MediaLibrary.h>
 #import <MapKit/MapKit.h>
 
+typedef enum    {
+                    MediaLibraryLoaded,
+                    RootMediaGroupLoaded,
+                    SubMediaGroupLoaded,
+                    MediaObjectsLoaded
+    
+                } MediaLoadingMessage;
+
 @interface MediaLibraryAccessor : NSObject {
     NSMutableArray *MediaObjects;
     MLMediaGroup *albums;
     bool Finished;
+    bool ErrorState;
     
+    NSInteger AlbumsToLoad;
+    NSInteger AlbumsLoaded;
+    
+    NSString *ErrorMessage;
     id Delegate;
     NSString *Selector;
+    NSMutableArray *KVO_Observers;
 }
 
 @property MLMediaLibrary *mediaLibrary;
 @property MLMediaGroup *allPhotosAlbum;
+@property bool ErrorState;
+@property NSString *ErrorMessage;
 
 - (void)initialize;
 
@@ -30,11 +46,40 @@
 - (void)setDelegate:(id)del withSelector:(NSString *)sel;
 
 - (NSMutableArray *)getMediaObjects;
+- (void)callDelegateAndExit;
+- (void)removeObserverFromMediaLibrary;
+- (void)reportErrorFindingMedia;
 
 + (NSDictionary *)getImagePropertiesDictionary:(NSURL *)path;
 + (NSString *)getExifDateTimeOriginal:(NSURL *)path;
 + (NSNumber *)getGpsAltitude:(NSURL *)path;
 + (NSNumber *)getGpsSpeed:(NSURL *)path;
 + (CLLocationCoordinate2D)getLocation:(NSURL *)path;
+
+@end
+
+@interface MediaObjectsLoadedMessenger : NSObject {
+    
+    MLMediaGroup *MediaGroup;
+    MediaLoadingMessage Message;
+}
+
+@property MLMediaGroup *MediaGroup;
+@property MediaLoadingMessage Message;
+
+@end
+
+@interface KVO_Observer : NSObject {
+    
+    id Observee;
+    id Observer;
+    NSString *KeyPath;
+}
+
+@property id Observee;
+@property id Observer;
+@property NSString *KeyPath;
+
+- (id)initWithObservee:(id)observee andKeyPath:(NSString *)keyPath andObserver:(id)observer;
 
 @end
